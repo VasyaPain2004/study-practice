54e6529c0c45facfa016396bce855f3412b3cfb3
Vasya <VasyaPain2004@gmail.com>
2025-08-06 17:42:49 +0900

    second task completed

---
+++ note-management-app/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from config import Config
+from models import db, Note
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/api/notes', methods=['POST'])
+def create_note():
+    data = request.json
+    new_note = Note(title=data['title'], content=data['content'])
+    db.session.add(new_note)
+    db.session.commit()
+    return jsonify(new_note.to_dict()), 201
+
+@app.route('/api/notes', methods=['GET'])
+def get_notes():
+    notes = Note.query.all()
+    return jsonify([note.to_dict() for note in notes])
+
+@app.route('/api/notes/<int:id>', methods=['GET'])
+def get_note(id):
+    note = Note.query.get_or_404(id)
+    return jsonify(note.to_dict())
+
+@app.route('/api/notes/<int:id>', methods=['PUT'])
+def update_note(id):
+    note = Note.query.get_or_404(id)
+    data = request.json
+    note.title = data.get('title', note.title)
+    note.content = data.get('content', note.content)
+    db.session.commit()
+    return jsonify(note.to_dict())
+
+@app.route('/api/notes/<int:id>', methods=['DELETE'])
+def delete_note(id):
+    note = Note.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
